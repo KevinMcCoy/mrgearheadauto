99e2b96a93a9eba8dbd613bacbdf4b701a222d3d
Kevin McCoy <KevinMcCoy@earthlink.net>
2014-12-02 23:45:15 -0500

    Copyright

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer id="colophon" role="contentinfo">
-	<div id="site-generator"></div>
+	<div id="site-generator">Mr. GearHead Auto &copy;2014</div>
 </footer>
 
 <div style="display:none"></div>

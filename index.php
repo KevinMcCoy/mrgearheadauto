5d1022c4490597f1caea32b93788915de8f21c09
Kevin McCoy <KevinMcCoy@earthlink.net>
2014-12-19 20:34:26 -0500

    video added

--- index.php
+++ index.php
@@ -18,6 +18,9 @@
 
 					<div class="entry-content">
 <p>Come visit Mr. Gearhead in <b>Colorado Springs</b>! We are proud to specialize in <b>Auto Repair</b>. As the premier auto repair shop in Colorado Springs, our highly-trained automotive mechanic takes great pride in supplying: <b>Engine Repair, Brake Repair, Auto Electrical Service, and Custom Exhaust Repair</b>.</p>
+
+<iframe src="https://fast.wistia.net/embed/iframe/35l5ivorm4?controlsVisibleOnLoad=true&playerColor=333333&plugin%5Bsocialbar-v1%5D%5BbadgeImage%5D=http%3A%2F%2Fembed.wistia.com%2Fdeliveries%2F3574f7f1910ee97739ad806a18fc577d0475301f.jpg%3Fimage_resize%3D100&plugin%5Bsocialbar-v1%5D%5BbadgeUrl%5D=http%3A%2F%2Fdatasphere.com%3Futm_source%3Dembedvideo%3Futm_source%3Dembedvideo&plugin%5Bsocialbar-v1%5D%5Bbuttons%5D=facebook-twitter&plugin%5Bsocialbar-v1%5D%5Blogo%5D=true&plugin%5Bsocialbar-v1%5D%5BpageUrl%5D=http%3A%2F%2Fmy.datasphere.com%2Fnode%2F2338234%3Futm_source%3Dembedvideo&version=v1&videoHeight=360&videoWidth=640&volumeControl=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" width="640" height="388"></iframe>
+
 <p>Mr. Gearhead focuses on Auto Repair, Engine Repair, Brake Repair, Auto Electrical Service and Custom Exhaust Repair; we also provide a long range of other auto repair services listed on our <a href="services.php">services page</a>.</p>
 <p>No matter what your vehicle needs, come see us at Mr. Gearhead Auto Repair today! From Brakes to transmission to engine work, we are the best one stop auto repair shop in Colorado Springs. We provide trustworthy, fast, reliable, and quality work at the right price. Don’t just pass through; come see us for any of your auto repair needs.</p>
 					</div>
---
+++ video.php
@@ -0,0 +1,22 @@
+<?php $thisPage = 'videos'; include('head.php'); ?>
+
+<body class="payment-methods custom-background">
+
+	<div id="page-wrap" class="contain">
+
+	<?php include('nav.php'); ?>
+
+		<div id="container" class="contain">
+
+			<iframe src="https://fast.wistia.net/embed/iframe/35l5ivorm4?controlsVisibleOnLoad=true&playerColor=333333&plugin%5Bsocialbar-v1%5D%5BbadgeImage%5D=http%3A%2F%2Fembed.wistia.com%2Fdeliveries%2F3574f7f1910ee97739ad806a18fc577d0475301f.jpg%3Fimage_resize%3D100&plugin%5Bsocialbar-v1%5D%5BbadgeUrl%5D=http%3A%2F%2Fdatasphere.com%3Futm_source%3Dembedvideo%3Futm_source%3Dembedvideo&plugin%5Bsocialbar-v1%5D%5Bbuttons%5D=facebook-twitter&plugin%5Bsocialbar-v1%5D%5Blogo%5D=true&plugin%5Bsocialbar-v1%5D%5BpageUrl%5D=http%3A%2F%2Fmy.datasphere.com%2Fnode%2F2338234%3Futm_source%3Dembedvideo&version=v1&videoHeight=360&videoWidth=640&volumeControl=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" width="640" height="388"></iframe>
+
+			<?php include('sidebar.php'); ?>
+
+
+		</div><!-- end container -->
+	</div><!-- end page-wrap -->
+
+<?php include('footer.php'); ?>
+
+</body>
+</html>

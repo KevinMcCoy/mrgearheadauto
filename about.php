49247e6aa5bdc1a956fa0a3358d643ef315896de
Kevin McCoy <KevinMcCoy@earthlink.net>
2015-06-14 21:04:57 -0400

    Remove pic

--- about.php
+++ about.php
@@ -24,15 +24,15 @@
 <h3>Services and Specialties</h3>
 <p>Mr. Gearhead is pleased to offer a wide variety of the most commonly requested auto maintenance and auto repair services which include: oil changes, brake services, tire rotations, timing belt replacements, and many more.</p>
 <p>In addition to the services mentioned above, we specialize in several types of truck and auto repair service, including: Auto Repair, Engine Repair, Brake Repair, Auto Electrical Service and Custom Exhaust Repair</p>
-<img class="" src="images/about/2.png" alt="Store Front Window" height="678" width="300">
+<!-- <img class="" src="images/about/2.png" alt="Store Front Window" height="678" width="300"> -->
 
 <span>
 	<h1>Contact Us...<h1>
 	<h2>We're Here to Help!</h2>
 </span>
 
-<p>Call us at <span>(719) 264-1022</span> or email us 
-<a href="mailto:mr.gearheadllc@gmail.com?subject=Inquire to Mr. Gearhead Auto">Mr.Gearheadllc&commat;gmail.com</a> 
+<p>Call us at <span>(719) 264-1022</span> or email us
+<a href="mailto:mr.gearheadllc@gmail.com?subject=Inquire to Mr. Gearhead Auto">Mr.Gearheadllc&commat;gmail.com</a>
  to inquire about how we can help with your specific automobile concerns. And if you're in the Colorado Springs area, feel free to stop by the shop and say hello, even if you aren't currently in need of auto repair or maintenance services. We'd be delighted to introduce ourselves, show you around the facility and answer any questions you may have about our business and our services.</p>
 
 

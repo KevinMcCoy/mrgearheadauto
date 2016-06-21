7d969ad10b55293ccf66413791caef6140a70a1a
Kevin McCoy <mccoy@fullsail.edu>
2016-06-21 00:53:00 -0400

    Updated the copyright wording

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer id="colophon" role="contentinfo">
-	<div id="site-generator">Mr. GearHead Auto &copy;2014-2016 </div>
+	<div id="site-generator">Copright &copy; 2014-2016 Mr. GearHead Auto LLC</div>
 </footer>
 
 <div style="display:none"></div>

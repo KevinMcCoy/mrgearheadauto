356f0cbc7724a043e3f79f219e2bbd30a6f37705
Kevin McCoy <mccoy@fullsail.edu>
2017-09-20 09:51:58 -0400

    page updated for address and year

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer id="colophon" role="contentinfo">
-	<div id="site-generator">Copright &copy; 2014-2016 Mr. GearHead Auto LLC</div>
+	<div id="site-generator">Copright &copy; 2014-<?php echo date("Y"); ?> Mr. GearHead Auto LLC</div>
 </footer>
 
 <div style="display:none"></div>
--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 	<!-- Box One Map -->
 	<li id="widget_contact_info-2" class="widget widget_contact_info">
 		<h2 class="widget-title">Hours &amp; Info</h2>
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.514513739922!2d-104.81589989999999!3d38.91221600000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87134e4c093f87ed%3A0x19ea46196368b362!2s5923+N+Nevada+Ave%2C+Colorado+Springs%2C+CO+80918!5e0!3m2!1sen!2sus!4v1417493143842" width="405" height="450" frameborder="0" style="border:0"></iframe>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3104.514513739922!2d-104.81589989999999!3d38.91221600000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87134e4c093f87ed%3A0x19ea46196368b362!2s2725+North+Nevada+Avenue%2C+Colorado+Springs%2C+CO+80918!5e0!3m2!1sen!2sus!4v1417493143842" width="405" height="450" frameborder="0" style="border:0"></iframe>
 
 		<div id="contact-map">
 			<input type="hidden" id="contact-info-map-lat" value="38.9190456" />
@@ -11,8 +11,8 @@
 			<div id="contact-info-map-canvas"></div>
 		</div>
 		<div class="confit-address">
-			<a href="http://maps.google.com/maps?z=16&#038;q=5923+north+nevada+ave+colorado+springs,+colorado+80918" target="_blank">5923 North Nevada Ave
-			<br/>Colorado Springs, Colorado 80918</a>
+			<a href="http://maps.google.com/maps?z=16&#038;q=2725+north+nevada+ave+colorado+springs,+colorado+80907" target="_blank">2725 North Nevada Ave
+			<br/>Colorado Springs, Colorado 80907</a>
 			<br/>719-264-1022
 		</div>
 

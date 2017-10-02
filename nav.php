19ca264d981d07f938a480a548a13bede3bd0d7e
Kevin McCoy <mccoy@fullsail.edu>
2017-10-01 22:56:42 -0400

    update url remove php

--- nav.php
+++ nav.php
@@ -1,14 +1,14 @@
 <header id="branding" role="banner">
 	<div class="contain">
 		<div id="escapehatch">
-			<h1 id="site-title"><a href="index.php">Mr.Gearhead Auto</a></h1>
+			<h1 id="site-title"><a href="/">Mr.Gearhead Auto</a></h1>
 			<h2 id="site-phone">719-264-1022</h2>
 		</div>
 		<nav id="menu" role="navigation">
 			<ul>
-				<li<?php if ($thisPage=="home") echo " class=\"current_page_item\""; ?>><a href="index.php">Home</a></li>
-				<li<?php if ($thisPage=="about") echo " class=\"current_page_item\""; ?>><a href="about.php">About Mr. Gear Head Auto</a></li>
-				<li<?php if ($thisPage=="services") echo " class=\"current_page_item\""; ?>><a href="services.php">Services</a></li>
+				<li<?php if ($thisPage=="home") echo " class=\"current_page_item\""; ?>><a href="/">Home</a></li>
+				<li<?php if ($thisPage=="about") echo " class=\"current_page_item\""; ?>><a href="about">About Mr. Gear Head Auto</a></li>
+				<li<?php if ($thisPage=="services") echo " class=\"current_page_item\""; ?>><a href="services">Services</a></li>
 				<!-- <li<?php //if ($thisPage=="payment-methods") echo " class=\"current_page_item\""; ?>><a href="payment-methods.php">Payment Methods</a></li> -->
 			</ul>
 		</nav>

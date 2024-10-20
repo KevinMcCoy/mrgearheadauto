a4aaf59592fd20b00e8d1fcf6c3541fb304c1cdd
KevinMcCoy <kevinwaterskier@gmail.com>
2024-10-20 09:42:05 -0400

    css print version added and cleanup
    removing wp stuff and bad meta

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 
 	<div id="page-wrap" class="contain">
 
-	<?php include('nav.php'); ?>
+		<?php include('nav.php'); ?>
 
 		<div id="container" class="contain">
 
@@ -35,10 +35,7 @@
 <p>Call us at <span>(719) 264-1022</span> or email us
 <a href="mailto:mr.gearheadllc@gmail.com?subject=Inquire%20to%20Mr.%20Gearhead%20Auto">Mr.Gearheadllc&commat;gmail.com</a>
  to inquire about how we can help with your specific automobile concerns. And if you're in the Colorado Springs area, feel free to stop by the shop and say hello, even if you aren't currently in need of auto repair or maintenance services. We'd be delighted to introduce ourselves, show you around the facility and answer any questions you may have about our business and our services.</p>
-
-
-
-					</div>
+					</div><!-- entry-content -->
 				</article>
 			</div><!-- end main -->
 
--- head.php
+++ head.php
@@ -11,11 +11,7 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Mr.Gearhead Auto LLC | Colorado Springs</title>
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-
-	<!--[if lt IE 9]>
-	<script src="http://s0.wp.com/wp-content/themes/pub/parament/js/html5.js?m=1316120706g" type="text/javascript"></script>
-	<![endif]-->
+	<!-- <link rel="profile" href="http://gmpg.org/xfn/11" /> -->
 
 <script type="text/javascript">
 /* <![CDATA[ */
@@ -23,17 +19,9 @@ function addLoadEvent(func){var oldonload=window.onload;if(typeof window.onload!
 /* ]]> */
 </script>
 
-<!--[if lt IE 8]>
-<link rel='stylesheet' id='highlander-comments-ie7-css'  href='http://s2.wp.com/wp-content/mu-plugins/highlander-comments/style-ie7.css?m=1351637563g&#038;ver=20110606' type='text/css' media='all' />
-<![endif]-->
-
-<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://mrgearheadauto.wordpress.com/xmlrpc.php?rsd" />
-<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://s1.wp.com/wp-includes/wlwmanifest.xml" />
-
 <meta name="generator" content="Hand Made" />
 <link rel='canonical' href='http://mrgearheadauto.com/' />
-<link rel='shortlink' href='http://45.55.57.206/mrgearheadauto.com/' />
-<!-- <link rel="alternate" type="application/json+oembed" href="https://public-api.wordpress.com/oembed/1.0/?format=json&amp;url=http%3A%2F%2Fmrgearheadauto.com%2F&amp;for=wpcom-auto-discovery" /><link rel="alternate" type="application/xml+oembed" href="https://public-api.wordpress.com/oembed/1.0/?format=xml&amp;url=http%3A%2F%2Fmrgearheadauto.com%2F&amp;for=wpcom-auto-discovery" /> -->
+<!-- <link rel='shortlink' href='http://45.##.###.206/mrgearhea.dauto.com/' /> -->
 
 <!-- Jetpack Open Graph Tags -->
 <meta property="og:type" content="website" />
@@ -41,15 +29,11 @@ function addLoadEvent(func){var oldonload=window.onload;if(typeof window.onload!
 <meta property="og:description" content="Get it right the first time" />
 <meta property="og:url" content="http://mrgearheadauto.com/" />
 <meta property="og:site_name" content="Mr. Gear Head Auto" />
-<meta property="og:image" content="https://s0.wp.com/i/blank.jpg" />
+<!-- <meta property="og:image" content="https://s0.wp.com/i/blank.jpg" /> -->
 
 <meta property="article:publisher" content="Kevin McCoy" />
-
-<link rel='openid.server' href='http://mrgearheadauto.wordpress.com/?openidserver=1' />
-<link rel='openid.delegate' href='http://mrgearheadauto.wordpress.com/' />
-<link rel="search" type="application/opensearchdescription+xml" href="http://mrgearheadauto.com/osd.xml" title="Mr. Gearhead Auto" />
-
-<meta name="application-name" content="Mr. Gear Head Auto" /><meta name="msapplication-window" content="width=device-width;height=device-height" /><meta name="msapplication-tooltip" content="Get it right the first time" /><meta name="msapplication-task" content="name=Subscribe;action-uri=http://mrgearheadauto.com/feed/;icon-uri=http://s2.wp.com/i/favicon.ico" /><meta name="msapplication-task" content="name=Sign up for a free blog;action-uri=http://wordpress.com/signup/;icon-uri=http://s2.wp.com/i/favicon.ico" /><meta name="msapplication-task" content="name=WordPress.com Support;action-uri=http://support.wordpress.com/;icon-uri=http://s2.wp.com/i/favicon.ico" /><meta name="msapplication-task" content="name=WordPress.com Forums;action-uri=http://forums.wordpress.com/;icon-uri=http://s2.wp.com/i/favicon.ico" /><meta name="title" content="Services | Mr. Gear Head Auto on WordPress.com" />
+<meta name="application-name" content="Mr. Gear Head Auto" />
+<meta name="title" content="Mr. Gear Head Auto" />
 <meta name="description" content="Mr. Gearhead offers an array of services for your automotive and truck needs. At Mr. Gearhead we care about you and your car and suggest that you take a serious look at preventative maintenance for the long-term welfare of your car and your pocketbook. Nothing keeps your automobile running optimally and safer than prevention and&hellip;" />
 
 	<!-- Fav Icon -->
@@ -58,10 +42,10 @@ function addLoadEvent(func){var oldonload=window.onload;if(typeof window.onload!
 	<link rel="apple-touch-icon-precomposed" href="images/logo.png" />
 
 	<!-- default CSS  -  Main.CSS -->
-	<link rel="stylesheet" type="text/css" media='all' href="css/_static.css">
-	<link rel="stylesheet" type="text/css" media='print' href="css/global-print.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" media='all' href="css/_static.css?v=<?= filemtime('css/_static.css')?>">
+	<link rel="stylesheet" type="text/css" media='print' href="css/global-print.css?v=<?= filemtime('css/_static.css')?>">
+	<link rel="stylesheet" type="text/css" href="css/style.css?v=<?= filemtime('css/_static.css')?>">
+	<link rel="stylesheet" type="text/css" href="css/main.css?v=<?= filemtime('css/_static.css')?>">
 
 	<!-- Google Plus Button - Place this tag in your head or just before your close body tag. -->
 	<script src="https://apis.google.com/js/platform.js" async defer></script>
